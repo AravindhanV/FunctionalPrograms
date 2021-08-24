4760eeb78753691cf955396b225463789545cfd4
Aravindhan V <aravindhan.v@perfios.com>
2021-08-24 07:32:23 +0530

    MODIFIED: 2D Array can read int, double and boolean arrays

--- src/com/bridgelabz/functionalprograms/TwoDimensionalArray.java
+++ src/com/bridgelabz/functionalprograms/TwoDimensionalArray.java
@@ -5,38 +5,113 @@ import java.io.*;
 
 public class TwoDimensionalArray {
 	public static void main(String[] args) {
-		int noOfRows, noOfColumns;
-		
+		int noOfRows, noOfColumns, choice;
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Number of Rows");
 		noOfRows = scanner.nextInt();
 		System.out.println("Number of Columns:");
 		noOfColumns = scanner.nextInt();
+		System.out.println("Enter 1. Integer\n2. Double\n3. Boolean");
+		choice = scanner.nextInt();
+
+		switch (choice) {
+		case 1:
+			int[][] intArray = readIntArray(scanner, noOfRows, noOfColumns);
+			printArray(intArray);
+			break;
+		case 2:
+			double doubleArray[][] = readDoubleArray(scanner, noOfRows, noOfColumns);
+			printArray(doubleArray);
+			break;
+		case 3:
+			boolean booleanArray[][] = readBooleanArray(scanner, noOfRows, noOfColumns);
+			printArray(booleanArray);
+			break;
+		}
+
 		
-		int[][] array = new int[noOfRows][noOfColumns];
+		scanner.close();
+
+		System.out.println();
+//		printArray(array);
+	}
+
+	public static int[][] readIntArray(Scanner scanner,int noOfRows,int noOfColumns) {
+		int[][] intArray = new int[noOfRows][noOfColumns];
 		System.out.println("Enter contents of array:");
-		
-		for(int i=0;i<noOfRows;i++) {
-			for(int j=0;j<noOfColumns;j++) {
-				array[i][j] = scanner.nextInt();
+
+		for (int i = 0; i < noOfRows; i++) {
+			for (int j = 0; j < noOfColumns; j++) {
+				intArray[i][j] = scanner.nextInt();
 			}
 		}
-		scanner.close();
 		
-		System.out.println();
-		printArray(array);
+		return intArray;
 	}
 	
-	public static void printArray(int [][] array) {
-		PrintWriter printWriter = new PrintWriter(System.out);
+	public static double[][] readDoubleArray(Scanner scanner,int noOfRows,int noOfColumns) {
+		double[][] doubleArray = new double[noOfRows][noOfColumns];
+		System.out.println("Enter contents of array:");
+
+		for (int i = 0; i < noOfRows; i++) {
+			for (int j = 0; j < noOfColumns; j++) {
+				doubleArray[i][j] = scanner.nextDouble();
+			}
+		}
 		
-		for(int i=0;i<array.length;i++) {
-			for(int j=0;j<array[0].length;j++) {
-				printWriter.append(array[i][j]+" ");
+		return doubleArray;
+	}
+	
+	public static boolean[][] readBooleanArray(Scanner scanner,int noOfRows,int noOfColumns) {
+		boolean[][] booleanArray = new boolean[noOfRows][noOfColumns];
+		System.out.println("Enter contents of array:(T for true and F for false");
+
+		for (int i = 0; i < noOfRows; i++) {
+			for (int j = 0; j < noOfColumns; j++) {
+				booleanArray[i][j] = scanner.next().equalsIgnoreCase("T") ? true : false;
+			}
+		}
+		
+		return booleanArray;
+	}
+
+	public static void printArray(int[][] array) {
+		PrintWriter printWriter = new PrintWriter(System.out);
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				printWriter.append(array[i][j] + " ");
 			}
 			printWriter.append("\n");
 		}
-		
+
+		printWriter.flush();
+	}
+	
+	public static void printArray(double[][] array) {
+		PrintWriter printWriter = new PrintWriter(System.out);
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				printWriter.append(array[i][j] + " ");
+			}
+			printWriter.append("\n");
+		}
+
+		printWriter.flush();
+	}
+	
+	public static void printArray(boolean[][] array) {
+		PrintWriter printWriter = new PrintWriter(System.out);
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				printWriter.append(array[i][j] + " ");
+			}
+			printWriter.append("\n");
+		}
+
 		printWriter.flush();
 	}
 }
